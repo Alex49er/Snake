700b7ebf5f47539c61143fbdbde939e0bade0e24
Alex49er <151443937+Alex49er@users.noreply.github.com>
2024-06-19 16:54:45 +0100

    started adding logic to the snake game

--- Snake/Direction.cs
+++ Snake/Direction.cs
@@ -1,4 +1,5 @@
-﻿namespace Snake
+﻿
+namespace Snake
 {
     public class Direction
     {
@@ -14,6 +15,33 @@
         {
             RowOffset = rowOffset;
             ColOffset = colOffset;
+        }
+
+        public Direction Opposite()
+        {
+            return new Direction(-RowOffset, -ColOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColOffset == direction.ColOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColOffset);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
         }
     }
 }
